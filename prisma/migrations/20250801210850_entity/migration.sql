3820af420ce4119e073e5271bc9c0c3a4c1e7682
Xmayora <xmayora@wingsoft.com>
2025-08-01 17:18:06 -0400

    new entity

---
+++ prisma/migrations/20250801210850_entity/migration.sql
@@ -0,0 +1,18 @@
+-- AlterTable
+ALTER TABLE "public"."Products" ADD COLUMN     "buysId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "public"."Buys" (
+    "id" SERIAL NOT NULL,
+    "createdAT" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAT" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER,
+
+    CONSTRAINT "Buys_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."Products" ADD CONSTRAINT "Products_buysId_fkey" FOREIGN KEY ("buysId") REFERENCES "public"."Buys"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Buys" ADD CONSTRAINT "Buys_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
