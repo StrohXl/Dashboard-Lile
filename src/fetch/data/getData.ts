84d20cd9d7112ac0c0315ea872bb067cfbb5d30e
StrohXl <xaviermayora20@gmail.com>
2025-08-14 19:34:00 -0400

    replace variable env

--- src/fetch/data/getData.ts
+++ src/fetch/data/getData.ts
@@ -16,9 +16,23 @@ const getData = async ({
   const myToken: RequestCookie | undefined = cookieStore.get("myToken");
 
   // Obtener url de dominio
+  const node_env = process.env.DEPLOY_SITE || "";
+  let siteUrl = "";
+  if (node_env == "development") {
+    siteUrl = process.env.URL_DEV || "http://localhost:3000";
+    console.log("development", siteUrl);
+  } else {
+    // En producción, asegúrate de incluir el protocolo
+    const host = (await headers()).get("host") as string;
+    siteUrl = `https://${host}`; // Asume HTTPS en producción
+    console.log("prod", siteUrl);
+    const protocol =
+      (await headers()).get("x-forwarded-proto") === "https" ? "https" : "http";
+    siteUrl = `${protocol}://${host}`;
+  }
 
   try {
-    const { data } = await axios.get(`/api${url}`, {
+    const { data } = await axios.get(`${siteUrl}/api${url}`, {
       params,
       headers: {
         Cookie: `${myToken?.name}=${myToken?.value}`,
