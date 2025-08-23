49be238dc980a6d7bcf0a7387f11e0c739a12148
StrohXl <xaviermayora20@gmail.com>
2025-08-23 10:45:34 -0400

    added new fecth getPyDollar

--- src/fetch/pydolar/getPyDolar.ts
+++ src/fetch/pydolar/getPyDolar.ts
@@ -8,7 +8,7 @@ type PyDolar = {
   };
 };
 
-const getPyDollar = async (): Promise<number | undefined> => {
+const getPyDollarVe = async (): Promise<number | undefined> => {
   try {
     const { data }: { data: PyDolar | undefined } = await axios.get(
       "https://pydolarve.org/api/v2/tipo-cambio"
@@ -16,6 +16,28 @@ const getPyDollar = async (): Promise<number | undefined> => {
     return data?.monitors.usd.price;
   } catch (error) {
     console.log(error);
+    throw new Error();
   }
 };
+
+const getDolarApi = async (): Promise<number | undefined> => {
+  try {
+    const { data }: { data: { promedio: number } | undefined } =
+      await axios.get("https://ve.dolarapi.com/v1/dolares/oficial");
+    return data?.promedio;
+  } catch (error) {
+    console.log(error);
+    throw new Error();
+  }
+};
+
+async function getPyDollar() {
+  const dolarApi = (await getDolarApi()) ?? new Error();
+  if (dolarApi instanceof Error) {
+    const pydollarVe = await getPyDollarVe();
+    return pydollarVe;
+  }
+  return dolarApi;
+}
+
 export default getPyDollar;
