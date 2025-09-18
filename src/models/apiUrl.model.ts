8812dba643c620f9f82317d26f1d02d76b6ee10f
StrohXl <xaviermayora20@gmail.com>
2025-09-18 11:47:43 -0400

    added history price

--- src/models/apiUrl.model.ts
+++ src/models/apiUrl.model.ts
@@ -1,1 +1,1 @@
-export type ApiUrl = "/buys" | "/clients" | "/products" | "/sales" | "/payments" | "/change_manager";
+export type ApiUrl = "/buys" | "/clients" | "/products" | "/sales" | "/payments" | "/change_manager" | "/history_price";
