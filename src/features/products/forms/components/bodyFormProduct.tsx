b4c947c167133c52a424c8051e63ff2105b380f9
StrohXl <xaviermayora20@gmail.com>
2025-09-19 18:08:55 -0400

    changing grid

--- src/features/products/forms/components/bodyFormProduct.tsx
+++ src/features/products/forms/components/bodyFormProduct.tsx
@@ -17,23 +17,21 @@ export default function BodyFormProduct({
 }) {
   return (
     <div className="body-form flex flex-col gap-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[1fr_1fr_1fr_1fr] gap-4">
-        <div className="lg:col-start-1 lg:col-end-3">
-          <InputFormProduct
-            error={errors.name}
-            label="Nombre del Producto"
-            nameField="name"
-            register={register}
-            options={{
-              required: "Este campo es requerido",
-              minLength: {
-                value: 3,
-                message: "El nombre debe de tener minimo 3 caracteres",
-              },
-            }}
-          />
-        </div>
-        <div className="lg:col-start-3 lg:col-end-4">
+      <div className="grid grid-cols-1 gap-4">
+        <InputFormProduct
+          error={errors.name}
+          label="Nombre del Producto"
+          nameField="name"
+          register={register}
+          options={{
+            required: "Este campo es requerido",
+            minLength: {
+              value: 3,
+              message: "El nombre debe de tener minimo 3 caracteres",
+            },
+          }}
+        />
+        <div className="grid md:grid-cols-2 gap-4  2xl:grid-cols-1">
           <SelectFormProduct
             error={errors.unit}
             label="Tipo"
@@ -45,8 +43,6 @@ export default function BodyFormProduct({
               { title: "Kg", value: "kg" },
             ]}
           />
-        </div>
-        <div className="lg:col-start-4 lg:col-end-5 ">
           <InputFormProduct
             error={errors.stock}
             label="Cantidad"
@@ -64,57 +60,50 @@ export default function BodyFormProduct({
             }}
           />
         </div>
-        <div className="lg:col-start-1 lg:col-end-5 ">
-          <span
-            className={`font-roboto block mb-2  ${
-              errors.price || errors.priceBs ? "text-red-500" : "text-gray-700"
-            }`}
-          >
-            Precio:
-          </span>
-          <div className="grid md:grid-cols-2 gap-4">
-            <InputFormProduct
-              error={errors.price}
-              nameField="price"
-              register={register}
-              placeholder="1"
-              step="any"
-              type="number"
-              defaultValue={0}
-              options={{
-                required: "Este campo es requerido",
-                min: {
-                  value: 0.1,
-                  message: "La cantidad debe de ser  minimo 0.1",
-                },
-                onChange: (event) =>
-                  setValue(
-                    "priceBs",
-                    Number(
-                      (Number(event.target.value) * Number(dollar)).toFixed(2)
-                    )
-                  ),
-              }}
-              iconEnd={<LuDollarSign />}
-            />
-            <InputFormProduct
-              error={errors.priceBs}
-              nameField="priceBs"
-              register={register}
-              defaultValue={0}
-              placeholder="1"
-              type="number"
-              step="any"
-              options={{
-                onChange: (event) =>
-                  setValue(
-                    "price",
-                    Number((event.target.value / Number(dollar)).toFixed(2))
-                  ),
-              }}
-              iconEnd={<span>Bs</span>}
-            />
-          </div>
+        <div className="grid md:grid-cols-2 gap-4">
+          <InputFormProduct
+            error={errors.price}
+            nameField="price"
+            register={register}
+            placeholder="1"
+            step="any"
+            type="number"
+            label="Precio en $"
+            defaultValue={0}
+            options={{
+              required: "Este campo es requerido",
+              min: {
+                value: 0.1,
+                message: "La cantidad debe de ser  minimo 0.1",
+              },
+              onChange: (event) =>
+                setValue(
+                  "priceBs",
+                  Number(
+                    (Number(event.target.value) * Number(dollar)).toFixed(2)
+                  )
+                ),
+            }}
+            iconEnd={<LuDollarSign />}
+          />
+          <InputFormProduct
+            error={errors.priceBs}
+            nameField="priceBs"
+            register={register}
+            defaultValue={0}
+            placeholder="1"
+            type="number"
+            step="any"
+            label="Precio en Bs"
+            options={{
+              onChange: (event) =>
+                setValue(
+                  "price",
+                  Number((event.target.value / Number(dollar)).toFixed(2))
+                ),
+            }}
+            iconEnd={<span>Bs</span>}
+          />
         </div>
       </div>
     </div>
