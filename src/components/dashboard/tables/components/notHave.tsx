374739aada5270a9a117a3f4a19d9a018ca6d29a
StrohXl <xaviermayora20@gmail.com>
2025-10-05 16:32:01 -0400

    added new params in the component NotHave

--- src/components/dashboard/tables/components/notHave.tsx
+++ src/components/dashboard/tables/components/notHave.tsx
@@ -3,14 +3,18 @@ import { IconType } from "react-icons/lib";
 export default function NotHave({
   icon,
   message,
+  size = 200,
+  height = 320,
 }: {
   icon: IconType;
   message: string;
+  size?: number;
+  height?: number;
 }) {
   const Icon = icon;
   return (
-    <div className="pb-3 h-80 flex flex-col justify-center items-center">
-      <Icon size={200} className="text-gray-500" />
+    <div className={`pb-3 h-[${height}px]  my-auto flex flex-col justify-center items-center`}>
+      <Icon size={size} className="text-gray-500" />
       <h4 className="font-open_sans text-lg mt-3  text-gray-700">{message}</h4>
     </div>
   );
